f7fa11d85d36eb170ed5d4225bd1024a46c1ec64
Kaif Ahmad <kaifhesham@gmail.com>
2023-10-05 03:05:24 +0530

    as

---
+++ src/firebase.js
@@ -0,0 +1,41 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER,
+  appId: process.env.REACT_APP_APP_ID
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCWv7pxBGFQ5UFLhjkJrAbIIKI9OGd56Bw",
+//   authDomain: "netflix-react-129d9.firebaseapp.com",
+//   projectId: "netflix-react-129d9",
+//   storageBucket: "netflix-react-129d9.appspot.com",
+//   messagingSenderId: "931196589147",
+//   appId: "1:931196589147:web:9caba767af1fc03e339b7e"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
